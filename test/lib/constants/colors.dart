5168ec44f352afb90f6111c585addda6160cd97b
hades510 <85429032+hades510@users.noreply.github.com>
2024-06-18 22:39:37 +0545

    Delete test directory

--- test/lib/constants/colors.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-Color background_color = Color.fromARGB(255, 237, 235, 228);
-Color appbar_color = Color.fromARGB(255, 215, 173, 49);
-
-TextStyle txtsty = TextStyle(
-  fontSize: 20,
-  color: Colors.amber,
-);
